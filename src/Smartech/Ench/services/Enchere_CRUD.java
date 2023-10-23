8b6c00e7fffb5c072a012364abbfc9d524015af0
Myacinech <yacin@192.168.0.6>
2023-10-23 20:28:42 +0100

    crud

--- src/Smartech/Ench/services/Enchere_CRUD.java
+++ src/Smartech/Ench/services/Enchere_CRUD.java
@@ -7,6 +7,7 @@ package Smartech.Ench.services;
 
 import Smartech.Ench.entities.Enchere;
 import Smartech.Ench.tools.MyBD;
+import com.mysql.jdbc.Statement;
 import java.sql.PreparedStatement;
 
 /**
@@ -14,6 +15,27 @@ import java.sql.PreparedStatement;
  * @author yacin
  */
 public class Enchere_CRUD 
+        
 {
-
+    
+    public void ajouterEnchere(Enchere e ){
+        String requete = "INSERT INTO Enchere (Titre, Id, Description, dateDebut, dateFin, prix_initial) VALUES  ('Voirie','12354789',' travaux de chaussee d'un quartier residentiel','10/02/2023','10/03/2023','250000')"; 
+        Statement st= new MyBD().getCnx().createStatement()
+    }
+    
+    public void ajouterEnchere(Enchere e ){
+        
+    }
+    public List<Enchere> afficherEnchere(){
+        
+    }
+    public void modifierEnchere(){
+        
+    }
+    public void supprimerEnchere(){
+        
+    }
+    public void rechercherEnchere(){
+        
+    }
 }
